fa135fe3496c11d8d855c44159b6415df11e27b4
ThaoVy1203 <thaovyn0312@gmail.com>
2025-10-18 09:22:47 +0700

    Hoàn thành bài KTGK

--- app/src/main/java/com/example/ktgk_dtdd/MainActivity.java
+++ app/src/main/java/com/example/ktgk_dtdd/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,16 +15,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button btnKQ;
+    EditText   edtHoTen, edtMSV, edtDTB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         btnKQ = findViewById(R.id.btnKQ);
+        edtDTB = findViewById(R.id.edtDTB);
+        edtMSV = findViewById(R.id.edtMSV);
+        edtHoTen = findViewById(R.id.edtHoTen);
                 btnKQ.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        startActivity(new Intent(MainActivity.this, ResultActivity.class));
+                        String DTBStr = edtDTB.getText().toString();
+                        String hoTenStr = edtHoTen.getText().toString();
+                        String MSVStr = edtMSV.getText().toString();
+
+                        if(hoTenStr.isEmpty() || MSVStr.isEmpty() || DTBStr.isEmpty()){
+                            Toast.makeText(MainActivity.this, "Vui lòng điền đủ thông tin!", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        double DTB = Double.parseDouble(DTBStr);
+                        Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                        intent.putExtra("HoTen", hoTenStr);
+                        intent.putExtra("MSV", MSVStr);
+                        intent.putExtra("DTB", DTB);
+                        startActivity(intent);
                     }
                 });
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
--- app/src/main/java/com/example/ktgk_dtdd/ResultActivity.java
+++ app/src/main/java/com/example/ktgk_dtdd/ResultActivity.java
@@ -1,6 +1,10 @@
 package com.example.ktgk_dtdd;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,39 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ResultActivity extends AppCompatActivity {
-
+    Button btnBack;
+    TextView tvKQ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_result);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnBack = findViewById(R.id.btnBack);
+        tvKQ = findViewById(R.id.tvKQ);
+
+        Intent intent = getIntent();
+        String hoTen = getIntent().getStringExtra("hoTen");
+        String MSV = getIntent().getStringExtra("MSV");
+        double DTB = getIntent().getDoubleExtra("DTB", 0);
+        String hocLuc;
+        if(DTB >= 8.5) hocLuc = "Giỏi";
+        else if(DTB >=6.5) hocLuc = "Khá";
+        else if(DTB >= 5.0) hocLuc = "Trung bình";
+        else hocLuc = "Yếu";
+        tvKQ.setText("Họ tên: " + hoTen + "\nMSV: "+ MSV
+            +"\nĐiểm trung bình: " + DTB
+            +"\nHọc lực: " +hocLuc);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResultActivity.this, MainActivity.class));
+            }
         });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
